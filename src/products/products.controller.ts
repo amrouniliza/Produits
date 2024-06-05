7e3c378f62b7365b1c864a82a1fc5f76ae89b41f
amrouniliza <amrouniliza37@gmail.com>
2024-06-05 23:07:21 +0200

    test sonarCloud

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -28,31 +28,6 @@ export class ProductsController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
-    console.log('id', id);
     return this.productsService.remove(+id);
   }
 }
