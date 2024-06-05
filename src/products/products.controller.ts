4e398ab62088cccd350000dba6330cf303ba8f4b
amrouniliza <amrouniliza37@gmail.com>
2024-06-05 23:09:56 +0200

    test sonarCloud

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,7 +4,7 @@ import { Prisma } from '@prisma/client';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
   create(@Body() createProductDto: Prisma.ProductCreateInput) {
@@ -28,6 +28,24 @@ export class ProductsController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    if (!id) {
+      console.log('No id provided')
+    }
+    if (!id) {
+      console.log('No id provided')
+    }
+    if (!id) {
+      console.log('No id provided')
+    }
+    if (!id) {
+      console.log('No id provided')
+    }
+    if (!id) {
+      console.log('No id provided')
+    }
+    if (!id) {
+      console.log('No id provided')
+    }
     return this.productsService.remove(+id);
   }
 }
