88d650acc41bf7a7b28834f48cb1d48f3eeb7709
amrouniliza <amrouniliza37@gmail.com>
2024-06-06 15:10:54 +0200

    test sonar connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,13 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
+    if (true) {
+      console.log('Hello');
+    }
     return this.appService.getHello();
   }
 }
